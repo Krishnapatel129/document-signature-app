d47db57e864dea1de86e9ac9f860ae1aa17cc778
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-06-09 13:27:37 +0530

    Day 1Task

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
+dns.setServers([
+  "8.8.8.8",
+  "8.8.4.4",
+  "1.1.1.1",
+  "1.0.0.1",
+]);
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Document Signature API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
