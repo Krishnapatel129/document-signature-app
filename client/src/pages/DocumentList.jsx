9b7dcb3996d10b5193252b727a5de9d7273d259a
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-06-27 23:33:47 +0530

    Fix authRoutes import variable name

--- client/src/pages/DocumentList.jsx
+++ client/src/pages/DocumentList.jsx
@@ -58,12 +58,7 @@ function DocumentList() {
           >
             Request Signature
           </button>
-          <button
-  onClick={() => onDelete(document._id)}
-  className="bg-red-600 text-white px-4 py-2 rounded-lg mt-3"
->
-  Delete
-</button>
+         
         </div>
       ))}
     </div>
