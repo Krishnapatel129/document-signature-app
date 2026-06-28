ac262d884f0db5b718826f8b6fe8fe46de731c80
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-06-28 14:47:01 +0530

    Fix API base URL slash

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,16 +2,11 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
-  connectionTimeout: 20000,
-  greetingTimeout: 20000,
-  socketTimeout: 20000,
+  port: 587,
+  secure: false,
+  family: 4, // force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
-export default transporter;
